42e18d230ed6f2d76d0ead6724dd22292d4f4ef9
kmusialski <musialski.k@gmail.com>
2015-12-20 21:50:48 +0100

    drugi commit v. 0.2

--- src/TABLE.cpp
+++ src/TABLE.cpp
@@ -6,43 +6,41 @@ void greatestNumber(char *arrow,size_t arraySize)
     int y =0;
     int i = 0;
     int t = 1000;
-    int z = 1
+    int z = 0;
     int suma = 0;
     int wynik = 0;
-
-
-
-    for (i; (i <= arraySize);i++)
+do{
+    for (i; (i < arraySize);i++)
     {
-        z++
-        if (z == 4){
-            z = 1;
+       if (z == 4){
+           i = i -3;
+           z = 0;
+            t = 1000;
             break;
         }
         y = (arrow[i] - '0');
         y = y*t;
         t=t/10;
         suma = suma + y;
+        z++;
     }
-
-    i = i+1;
-    t = 1000;
-
     if(suma>=wynik){
         wynik=suma;
     }
+    suma = 0;
     continue;
-
+}while(i < arraySize);
+cout<<wynik<<endl;
 }
 
 
-int TABLE::digital(){
+int TABLE::digital(){cout<<wynik<<endl;
 
 
-char digits[] = "8492119599";
+char digits[] = "849211959925629991121231313215151515168484329995962323351921489494564894-5626185484999999915156151417415";
 	//int maxNumber = 0;
 char *arrow = digits;
-size_t arraySize = sizeof(digits)/sizeof(ala[0]);
+size_t arraySize = sizeof(digits)/sizeof(digits[0]);
 greatestNumber(arrow,arraySize); // Na ekranie pojawi sie liczba 94857
 
 
